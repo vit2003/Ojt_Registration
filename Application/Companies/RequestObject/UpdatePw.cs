4f2306954ca2940d1806c7a420e0706b9749bb3b
tai <taidmse130140@fpt.edu.vn>
2021-11-03 13:53:51 +0700

    Update password for Company

---
+++ Application/Companies/RequestObject/UpdatePw.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Companies.RequestObject
+{
+    public class UpdatePw
+    {
+        [Required]
+        public string Password { get; set; }
+    }
+}
