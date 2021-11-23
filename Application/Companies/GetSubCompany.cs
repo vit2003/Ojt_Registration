04446f3047f04d345366e07c6a1c150b5a0c3156
vinh le <vinhle2311@gmail.com>
2021-11-23 16:51:03 +0700

    update function get subcompany

--- Application/Companies/GetSubCompany.cs
+++ Application/Companies/GetSubCompany.cs
@@ -28,6 +28,15 @@ namespace Application.Companies
                 _context = context;
             }
 
+            public string getMajorName(List<Major> major_list, int id)
+            {
+                foreach(Major major in major_list)
+                {
+                    if (major.Id == id) return major.MajorName;
+                }
+                return null;
+            }
+
             public async Task<List<SubCompany>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var listcompany = await _context.Companies.Where(x => x.IsSubCompany == true).ToListAsync();
@@ -35,12 +44,24 @@ namespace Application.Companies
                 var result = new List<SubCompany>();
                 foreach (Company company in listcompany)
                 {
+                    var companyMajors = await _context.MajorCompanies.Include(x => x.Majors).Where(x => x.CompanyId == company.Id).ToListAsync();
+
+                    var major_list = await _context.Majors.ToListAsync();
+
+                    var majorNames = new List<string>();
+
+                    foreach(MajorCompany mc in companyMajors)
+                    {
+                        majorNames.Add(getMajorName(major_list, mc.MajorId));
+                    };
+
                     var returnCompany = new SubCompany
                     {
                         Address = company.Address,
                         CompanyName = company.CompanyName,
                         Id = company.Id,
-                        Website = company.WebSite
+                        Website = company.WebSite,
+                        MajorName = majorNames
                     };
                     result.Add(returnCompany);
                 }
